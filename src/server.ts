0db5b2e7e7d5e0671e9c18581521711fea8e240a
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2021-11-16 00:14:53 -0300

    Server port contingência

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log(`Server running in port ${process.env.PORT}!`);
 });
